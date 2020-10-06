4532a909aa176a47becb99f4ed36528b58766d2e
Hristiyan Dimitrov <hdimitrov1511@gmail.com>
2020-10-06 18:29:23 +0300

    Login/Logout logic

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,16 +3,27 @@ import { Formik, Form } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/core";
+import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrormap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = () => {
+  const [{}, register] = useRegisterMutation();
+  const router = useRouter();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrormap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
